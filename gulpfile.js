073526ad71b4b08d97801c2ea9aa40c4bf418a3a
cameronmoodley <moodley.cameron@gmail.com>
2018-09-04 09:43:46 +0200

    fix for gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('livereload', function (){
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass', 'livereload']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('./public/**/*', ['livereload']);
 });
 
