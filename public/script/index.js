52ebc50789e5675a47a4e093d3249138638ad4c8
hortasha <eidemartin_303@hotmail.com>
2018-09-05 12:27:00 +0200

    Created info button on home page

---
+++ public/script/index.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var dropdown = document.getElementById("dropdown");
+
+    if (dropdown.style.display == "none") {
+      dropdown.style.display = "inline";
+    } else {
+      dropdown.style.display = "none";      
+    }
+}
